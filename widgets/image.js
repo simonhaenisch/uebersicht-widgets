c4692d569bfdbb3fbe505eb7761b60a0b56283ce
simonhaenisch <simonhaenisch@users.noreply.github.com>
2018-04-17 09:31:20 +1200

    add image widget

---
+++ widgets/image.js
@@ -0,0 +1,19 @@
+command: ``,
+
+refreshFrequency: false,
+
+render: res => `<img src="img.jpg">`,
+
+style: `
+bottom: 16px
+right: 20px
+height: calc(100% - 32px)
+width: calc(100% - 40px)
+
+img
+	position: absolute
+	bottom: 0
+	right: 0
+	height: 250px
+	border-radius: 5px
+`
